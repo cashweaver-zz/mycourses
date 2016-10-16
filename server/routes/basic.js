b1e9ac733fc3e46dbb4a178b3f9c89473904b124
cashweaver <cashbweaver@gmail.com>
2016-10-16 11:29:13 -0700

    Implement basic theme rendering

--- server/routes/basic.js
+++ server/routes/basic.js
@@ -1,23 +1,14 @@
 const express = require('express');
-
-// TODO: refactor this into a helper file
-// const templatePath = require.resolve('./../../public/themes/basic/index.pug');
-// const templateFn = require('pug').compileFile(templatePath);
-// const index = require.resolve('./../../public/themes/basic/index.pug');
-// const indexTemplate = require('pug').compileFile(templatePath);
-const theme = {
-  index: require('pug').compileFile(require.resolve('./../../public/themes/basic/index.pug')),
-};
+const render = require('./theme').render;
+const templates = require('./theme').templates;
 
 const router = express.Router();
 
 const handler = {
   renderRoot: (req, res) => {
-    //res.render('index', { pageTitle: 'Online Learning Platform' });
-    // ref: https://strongloop.com/strongblog/bypassing-express-view-rendering-for-speed-and-modularity/
-    res.set('Content-Type', 'text/html');
-    res.write(theme.index({ pageTitle: 'Online Learning Platform' }));
-    res.end();
+      render(res, templates.index({
+      pageTitle: 'Online Learning Platform',
+    }));
   },
 };
 
