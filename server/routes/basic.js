0523bf5f95f9b276abd07648d0872c38e418a172
cashweaver <cashbweaver@gmail.com>
2016-10-17 15:22:56 -0700

    Add course and setion detail views

--- server/routes/basic.js
+++ server/routes/basic.js
@@ -1,6 +1,7 @@
 const Course = require('./../db').Course;
 const express = require('express');
 const render = require('./theme').render;
+const Section = require('./../db').Section;
 const templates = require('./theme').templates;
 
 const router = express.Router();
@@ -11,18 +12,72 @@ const handler = {
       pageTitle: 'Online Learning Platform',
     }));
   },
-  renderCourses: (req, res) => {
+  renderCourses: (req, res) => (
     Course.findAll()
       .then((courses) => {
         render(res, templates.courses({
           courses,
-          pageTitle: 'Online Learning Platform',
+          pageTitle: 'Courses',
         }));
-      });
+      })
+  ),
+  renderCourse: (req, res) => (
+    Course.findOne({
+      where: {
+        id: req.params.courseId,
+      },
+      include: Section,
+    })
+    .then((course) => {
+      if (course !== null) {
+        render(res, templates.course({
+          course,
+          pageTitle: course.get('name'),
+        }));
+      } else {
+        res.status(404);
+        render(res, templates.notFound({
+          pageTitle: '404 Not Found',
+        }));
+      }
+    })
+  ),
+  renderSection: (req, res) => {
+    let section = null;
+    return Section.findOne({
+      where: {
+        id: req.params.sectionId,
+      },
+    })
+    .then((dbSection) => {
+      section = dbSection;
+
+      return Course.findOne({
+        where: {
+          id: req.params.courseId,
+        },
+      })
+    })
+    .then((course) => {
+      if (section !== null) {
+        render(res, templates.section({
+          course,
+          section,
+          pageTitle: section.get('name'),
+        }));
+      } else {
+        res.status(404);
+        render(res, templates.notFound({
+          pageTitle: '404 Not Found',
+        }));
+      }
+    })
   },
 };
 
 router.get('/', handler.renderRoot);
 router.get('/courses', handler.renderCourses);
+router.get('/courses/:courseId', handler.renderCourse);
+router.get('/courses/:courseId/sections/:sectionId', handler.renderSection);
 
 module.exports = { router, handler };
--- server/routes/basic.test.js
+++ server/routes/basic.test.js
@@ -1,11 +1,34 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-underscore-dangle */
 const basic = require('./basic');
+const db = require('./../db');
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const TestPath = require('./testHelpers').TestPath;
 
+const devCourses = [
+  { name: 'My First Course', body: 'It is going to be great!' },
+  { name: 'My Second Course', body: 'It is going to be great!' },
+  { name: 'My Third Course', body: 'It is going to be great!' },
+];
+const devSections = [
+  { name: 'My First Section', body: 'It is going to be great!', courseId: 1 },
+  { name: 'My Second Section', body: 'It is going to be great, again!', courseId: 1 },
+  { name: 'My Third Section', body: 'It is going to be great, again!', courseId: 2 },
+];
+
+const resetTableToDevData = (done) => {
+  // Drop, and re-create, the database
+  db.sequelize.sync({ force: true })
+    .then(() => db.Course.bulkCreate(devCourses))
+    .then(() => db.Section.bulkCreate(devSections))
+    .then(() => done())
+    .catch(err => done(err));
+};
+
 describe('basic', () => {
+  before(done => resetTableToDevData(done));
+
   describe('/', () => {
     const rootPath = new TestPath('/');
     rootPath.shouldHaveStatusCode('GET', 200);
@@ -28,13 +51,19 @@ describe('basic', () => {
     });
   });
 
+  describe('/someNonExistentEntpoint', () => {
+    const someNonExistentEntpoint = new TestPath('/someNonExistentEntpoint');
+    someNonExistentEntpoint.shouldHaveStatusCode('GET', 404);
+    someNonExistentEntpoint.shouldHaveContentType('GET', 'text/html');
+  });
+
   describe('/courses', () => {
     const coursesPath = new TestPath('/courses');
     coursesPath.shouldHaveStatusCode('GET', 200);
     coursesPath.shouldHaveContentType('GET', 'text/html');
 
     /** @todo: update test to ensure custom render function is called */
-    it('should respond with a rendered view', () => {
+    it('should respond with a rendered view', (done) => {
       const req = {};
       const res = {
         set: sinon.spy(),
@@ -42,12 +71,81 @@ describe('basic', () => {
         end: sinon.spy(),
       };
 
-      basic.handler.renderCourses(req, res);
-      expect(res.set.calledOnce).to.be.true;
-      expect(res.write.calledOnce).to.be.true;
-      expect(res.write.calledAfter(res.set)).to.be.true;
-      expect(res.end.calledOnce).to.be.true;
-      expect(res.end.calledAfter(res.write)).to.be.true;
+      basic.handler.renderCourses(req, res)
+        .then(() => {
+          expect(res.set.calledOnce).to.be.true;
+          expect(res.write.calledOnce).to.be.true;
+          expect(res.write.calledAfter(res.set)).to.be.true;
+          expect(res.end.calledOnce).to.be.true;
+          expect(res.end.calledAfter(res.write)).to.be.true;
+          done();
+        });
+    });
+  });
+
+  describe('/courses/1', () => {
+    const coursePath = new TestPath('/courses/1');
+    const nonExistentCoursePath = new TestPath('/courses/999');
+    coursePath.shouldHaveStatusCode('GET', 200);
+    nonExistentCoursePath.shouldHaveStatusCode('GET', 404);
+    coursePath.shouldHaveContentType('GET', 'text/html');
+    nonExistentCoursePath.shouldHaveContentType('GET', 'text/html');
+
+    /** @todo: update test to ensure custom render function is called */
+    it('should respond with a rendered view', (done) => {
+      const req = {
+        params: {
+          courseId: 1,
+        },
+      };
+      const res = {
+        set: sinon.spy(),
+        write: sinon.spy(),
+        end: sinon.spy(),
+      };
+
+      basic.handler.renderCourse(req, res)
+        .then(() => {
+          expect(res.set.calledOnce).to.be.true;
+          expect(res.write.calledOnce).to.be.true;
+          expect(res.write.calledAfter(res.set)).to.be.true;
+          expect(res.end.calledOnce).to.be.true;
+          expect(res.end.calledAfter(res.write)).to.be.true;
+          done();
+        });
+    });
+  });
+
+  describe('/courses/1/sections/1', () => {
+    const sectionPath = new TestPath('/courses/1/sections/1');
+    const nonExistentSectionPath = new TestPath('/courses/1/sections/999');
+    sectionPath.shouldHaveStatusCode('GET', 200);
+    nonExistentSectionPath.shouldHaveStatusCode('GET', 404);
+    sectionPath.shouldHaveContentType('GET', 'text/html');
+    nonExistentSectionPath.shouldHaveContentType('GET', 'text/html');
+
+    /** @todo: update test to ensure custom render function is called */
+    it('should respond with a rendered view', (done) => {
+      const req = {
+        params: {
+          courseId: 1,
+        },
+      };
+      const res = {
+        set: sinon.spy(),
+        write: sinon.spy(),
+        end: sinon.spy(),
+      };
+
+      basic.handler.renderCourse(req, res)
+        .then(() => {
+          expect(res.set.calledOnce).to.be.true;
+          expect(res.write.calledOnce).to.be.true;
+          expect(res.write.calledAfter(res.set)).to.be.true;
+          expect(res.end.calledOnce).to.be.true;
+          expect(res.end.calledAfter(res.write)).to.be.true;
+          done();
+        });
     });
   });
 });
