7646b84b5aef7947241f4372f079cd6bad80d42a
cashweaver <cashbweaver@gmail.com>
2016-10-17 12:12:54 -0700

    Add course list view

--- server/routes/basic.test.js
+++ server/routes/basic.test.js
@@ -1,25 +1,53 @@
 /* eslint-disable no-unused-expressions */
+/* eslint-disable no-underscore-dangle */
 const basic = require('./basic');
 const expect = require('chai').expect;
 const sinon = require('sinon');
 const TestPath = require('./testHelpers').TestPath;
 
 describe('basic', () => {
-  const root = new TestPath('/');
-  root.shouldHaveStatusCode('GET', 200);
-  root.shouldHaveContentType('GET', 'text/html');
+  describe('/', () => {
+    const rootPath = new TestPath('/');
+    rootPath.shouldHaveStatusCode('GET', 200);
+    rootPath.shouldHaveContentType('GET', 'text/html');
 
-  it('should respond at / with a rendered view', () => {
-    const req = {};
-    const res = {
-      end: sinon.spy(),
-      render: sinon.spy(),
-      set: sinon.spy(),
-      write: sinon.spy(),
-    };
+    /** @todo: update test to ensure custom render function is called */
+    it('should respond with a rendered view', () => {
+      const req = {};
+      const res = {
+        set: sinon.spy(),
+        write: sinon.spy(),
+        end: sinon.spy(),
+      };
+      basic.handler.renderRoot(req, res);
+      expect(res.set.calledOnce).to.be.true;
+      expect(res.write.calledOnce).to.be.true;
+      expect(res.write.calledAfter(res.set)).to.be.true;
+      expect(res.end.calledOnce).to.be.true;
+      expect(res.end.calledAfter(res.write)).to.be.true;
+    });
+  });
 
-    basic.handler.renderRoot(req, res);
-    // TODO: update test after refactoring away from res.render in favor of res.write.
-    expect(true).to.be.true;
+  describe('/courses', () => {
+    const coursesPath = new TestPath('/courses');
+    coursesPath.shouldHaveStatusCode('GET', 200);
+    coursesPath.shouldHaveContentType('GET', 'text/html');
+
+    /** @todo: update test to ensure custom render function is called */
+    it('should respond with a rendered view', () => {
+      const req = {};
+      const res = {
+        set: sinon.spy(),
+        write: sinon.spy(),
+        end: sinon.spy(),
+      };
+
+      basic.handler.renderCourses(req, res);
+      expect(res.set.calledOnce).to.be.true;
+      expect(res.write.calledOnce).to.be.true;
+      expect(res.write.calledAfter(res.set)).to.be.true;
+      expect(res.end.calledOnce).to.be.true;
+      expect(res.end.calledAfter(res.write)).to.be.true;
+    });
   });
 });
